944a314fef57bea10a7261a9ea05de888dac38b2
jwolfe1088 <maranara1088@gmail.com>
2026-04-15 18:10:12 -0400

    built conftest and api_client

---
+++ tests/ui_tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from pages.base_page import BasePage
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+from pages.notes_page import NotesPage
+
+
+
+@pytest.fixture()
+def base_page(page):
+    return BasePage(page)
+
+@pytest.fixture()
+def home_page(page):
+    return HomePage(page)
+
+@pytest.fixture()
+def login_page(page):
+    return LoginPage(page)
+
+@pytest.fixture()
+def notes_page(page):
+    return NotesPage(page)
+    
---
+++ utils/api_client.py
@@ -0,0 +1,53 @@
+import requests
+
+BASE_URL = "https://practice.expandtesting.com/notes/api"
+
+class APIClient:
+
+    def _get_headers(self, auth_token):
+        return {"x-auth-token": auth_token}
+
+    
+    
+    def user_login(self, email, password):
+        return requests.post(f"{BASE_URL}/users/login", json={"email": email, "password": password})
+
+
+
+    def user_register(self, name, email, password):
+        return requests.post(f"{BASE_URL}/users/register", json={"name": name, "email": email, "password": password})
+
+
+
+    def user_delete_account(self, auth_token):
+        return requests.delete(f"{BASE_URL}/users/delete-account", headers=self._get_headers(auth_token))
+
+
+
+    def create_new_note(self, title, description, category, auth_token):
+        return requests.post(f"{BASE_URL}/notes", json={"title": title, "description": description, "category": category}, headers=self._get_headers(auth_token))
+
+
+
+    def get_all_notes(self, auth_token):
+        return requests.get(f"{BASE_URL}/notes", headers=self._get_headers(auth_token))
+
+
+
+    def get_note_by_id(self, note_id, auth_token):
+        return requests.get(f"{BASE_URL}/notes/{note_id}", headers=self._get_headers(auth_token))
+
+
+
+    def update_note(self, note_id, title, description, status, category, auth_token):
+        return requests.put(f"{BASE_URL}/notes/{note_id}", json={"title": title, "description": description, "status": status, "category": category}, headers=self._get_headers(auth_token))
+
+
+
+    def update_status_of_note(self, note_id, status, auth_token):
+        return requests.patch(f"{BASE_URL}/notes/{note_id}", json={"status": status}, headers=self._get_headers(auth_token))
+
+
+
+    def delete_note(self, note_id, auth_token):
+        return requests.delete(f"{BASE_URL}/notes/{note_id}", headers=self._get_headers(auth_token))
