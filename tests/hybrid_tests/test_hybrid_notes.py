b2436f45cec359b605d701bb75a975ddeee167aa
jwolfe1088 <maranara1088@gmail.com>
2026-04-28 17:37:28 -0400

    finished hybrid tests and added github workflows page.

--- tests/hybrid_tests/test_hybrid_notes.py
+++ tests/hybrid_tests/test_hybrid_notes.py
@@ -20,4 +20,22 @@ def test_hybrid_status_of_note(api_client, auth_token, login_page, notes_page, e
     note = api_client.get_note_by_id(note_id, auth_token)
     assert note.json()["data"]["completed"] == True
     
+def test_hybrid_get_id(api_client, auth_token, login_page, notes_page, email, password, cleanup_note_api):
+    login_page.navigate_to_login()
+    login_page.login(email, password)
+    notes_page.click_add_note()
+    notes_page.select_new_note_category("Home")
+    notes_page.create_new_note_title("New note hybrid")
+    notes_page.create_new_note_description("Testing api get by id")
+    notes_page.click_note_submit()
+    all_notes = api_client.get_all_notes(auth_token)
+    note = [note for note in all_notes.json()["data"] if note["title"] == "New note hybrid"][0]
+    note_id = note["id"]
+    response = api_client.get_note_by_id(note_id, auth_token)
+    assert response.status_code == 200
+    cleanup_note_api(note_id)
+
+    
+    
+
   
